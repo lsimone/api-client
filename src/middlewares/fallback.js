4eac5ce8a966f7af76dd93fef7714910f9ab1274
lsimone <lorenzo.simone@musement.com>
2018-03-21 14:19:37 +0100

    re-using same res object without losing all res methods and props

--- src/middlewares/fallback.js
+++ src/middlewares/fallback.js
@@ -1,4 +1,4 @@
 export default function fallback ({options: {fallback}}, res) {
     return (fallback !== undefined && res.data === undefined)
-        ? {...res, data: fallback} : res
+        ? Object.assign(res, {data: fallback}) : res
 }
--- src/middlewares/mock.js
+++ src/middlewares/mock.js
@@ -2,9 +2,6 @@ export default function mock ({options: {mock: mockedRes}}, res) {
     // if mock===true, no change is needed: the request has been sent to mock server
     if (mockedRes && mockedRes !== true) {
         console.warn('*** mocked response', mockedRes)
-        return {
-            ...res,
-            data: mockedRes
-        }
+        return Object.assign(res, {data: mockedRes})
     } else return res
 }
