cecaf122137bda35562dfc9c1f876e1374c8eceb
lsimone <lorenzo.simone@musement.com>
2018-03-21 12:42:38 +0100

    fix res.json() promise

--- src/middlewares/status.js
+++ src/middlewares/status.js
@@ -1,5 +1,5 @@
 export default function status (req, res) {
     // returns res or a promise that resolves with res
     return (res.status >= 200 && res.status < 300)
-        ? res.json.then(data => ({...res, data})) : {...res, errorCode: res.status}
+        ? res.json().then(data => ({...res, data})) : {...res, errorCode: res.status}
 }
