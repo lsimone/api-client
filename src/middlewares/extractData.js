df919ec139f1b1e011e4347521beb1ed92885434
lsimone <lorenzo.simone@musement.com>
2018-04-09 20:01:08 +0200

    error object initialized

--- src/middlewares/extractData.js
+++ src/middlewares/extractData.js
@@ -1,5 +1,5 @@
 export default function extractData ({options: {fullResponse}}, res) {
     return fullResponse
         ? res : (res.data !== undefined)
-            ? Promise.resolve(res.data) : Promise.reject(res.errorCode)
+            ? Promise.resolve(res.data) : Promise.reject({status: res.errorCode})
 }
