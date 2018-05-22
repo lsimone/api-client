654562daa8a9e501a96dad9763381c7879af9625
lsimone <lorenzo.simone@musement.com>
2018-05-22 13:34:49 +0200

    empty body in success http call is now handled

--- src/middlewares/extractData.js
+++ src/middlewares/extractData.js
@@ -1,5 +1,5 @@
 export default function extractData ({options: {fullResponse}}, res) {
     return fullResponse
-        ? res : (res.data !== undefined)
+        ? res : res.ok
             ? Promise.resolve(res.data) : Promise.reject({status: res.errorCode, response: res})
 }
--- src/middlewares/status.js
+++ src/middlewares/status.js
@@ -1,9 +1,13 @@
 export default function status (req, res) {
     // returns res or a promise that resolves with res
-    return isError(res)
-        ? Object.assign(res, {errorCode: res.status}) : res.json().then(data => Object.assign(res, {data}))
+    return res.ok
+        ? extractJson(res, req.debug) : Object.assign(res, {errorCode: res.status})
 }
 
-function isError (res) {
-    return res instanceof Error || !(res.status >= 200 && res.status < 300)
+function extractJson (res, debug) {
+    return res.json().then(data => Object.assign(res, {data}))
+        .catch(e => {
+            debug && console.log('eeeeeeei', e)
+            return res
+        })
 }
